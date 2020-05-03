f678373327f3fb9807ddf482ae91a3516715f079
MsalgadoJ <mayra.rinconess@gmail.com>
2020-05-03 20:07:29 -0300

    MLv3

--- MLv3/controllers/productsController.js
+++ MLv3/controllers/productsController.js
@@ -33,21 +33,80 @@ const controller = {
 
 	// Create - Form to create
 	create: (req, res) => {
-		// Do the magic
+		res.render('product-create-form');
 	},
 	
 	// Create -  Method to store
 	store: (req, res) => {
-		// Do the magic
+		//res.send(req.body)
+		let newProduct = {}
+
+		if(products == ""){
+			newProduct.id=1
+		}else {
+			let lastProduct = products[products.length -1];
+			newProduct.id = lastProduct.id+1;
+		}
+				
+		newProduct.name = req.body.name;
+		newProduct.price = req.body.price;
+		newProduct.discount = req.body.discount;
+		newProduct.category = req.body.category;
+		newProduct.description = req.body.description;
+
+		products.push(newProduct);
+
+		let updatedProductsJSON = JSON.stringify(products);
+		fs.writeFileSync(productsFilePath,updatedProductsJSON);
+		
+		res.render('products',{
+			aMiles: toThousand,
+			products: products
+		})
+
 	},
 
 	// Update - Form to edit
 	edit: (req, res) => {
-		// Do the magic
+		let product = products.find(buscado =>{
+			return buscado.id == req.params.productId;
+		});
+
+		//console.log(product)
+
+		let title = 'Mercado Liebre Argentina | ' + product.name;
+		
+		res.render('product-edit-form',{
+			title: title,
+			aMiles:toThousand,
+			productToEdit: product
+		});
+
+
 	},
 	// Update - Method to update
 	update: (req, res) => {
-		// Do the magic
+		let productUpdated = {};
+
+		products.forEach(element => {
+			if(element.id == req.params.productId){
+				element.name = req.body.name;
+				element.price = req.body.price;
+				element.discount = req.body.discount;
+				element.category = req.body.category;
+				element.description = req.body.description;
+				return productUpdated = element;
+			}
+
+		});
+
+		let updatedProductsJSON = JSON.stringify(products);
+		fs.writeFileSync(productsFilePath,updatedProductsJSON);
+		
+		res.render('products',{
+			aMiles: toThousand,
+			products: products
+		})
 	},
 
 	// Delete - Delete one product from DB
@@ -58,7 +117,11 @@ const controller = {
 
 		let updatedProductsJSON = JSON.stringify(productsLeft);
 		fs.writeFileSync(productsFilePath,updatedProductsJSON);
-		res.send(productsLeft)
+		
+		res.render('products',{
+			aMiles: toThousand,
+			products: productsLeft
+		})
 	}
 };
 
